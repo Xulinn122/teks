7b63f7c08eb2b1d735a7430828d893b699c290ef
XulinnIsNotBlack <oncintudagm@gmail.com>
2025-11-18 15:04:08 -0300

    fonte_nome2

--- api/nome2.js
+++ api/nome2.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
 
     return res.status(200).json({
       sucesso: true,
-      fonte: "Bronxy (via proxy FVV API)",
+      fonte: "Xuslin",
       total: resultadoLimpo.length,
       resultados: resultadoLimpo
     });
